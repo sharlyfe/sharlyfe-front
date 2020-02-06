36ffa7db383dc66692377cf31b65f4b278d38bc0
sharlyfe <behnamsharlyfe@gmail.com>
2020-02-05 22:47:38 -0500

    initial commit for sharlyfe front

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import Navbar from "./components/navbar.component";
-import BlogsList from "./components/blog-list.component";
-import EditBlog from "./components/edit-blog.component";
-import CreateBlog from "./components/create-blog.component";
-import CreateUser from "./components/create-user.component";
+import "./App.css";
+import Home from "./containers/Home";
 function App() {
   return (
-    <Router>
-      {/* the following lines are all components that we should create , navbar is a component BlogList is a component and so on 
-    react route helps us to path in url in different component that we would create*/}
-      <div className="container">
-        <Navbar />
-        <br />
-        <Route path="/" exact component={BlogsList} />
-        <Route path="/edit/:id" exact component={EditBlog} />
-        <Route path="/create" exact component={CreateBlog} />
-        <Route path="/user" exact component={CreateUser} />
-      </div>
-    </Router>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
---
+++ src/containers/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import Header from "../../components/Header";
+const Home = props => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+export default Home;
